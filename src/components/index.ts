0ba9e8d78e19ed7846565bb9f084d7f1e637b4a8
kangyh427 <kangyh427@gmail.com>
2026-02-08 16:03:41 +0900

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,19 +1,23 @@
 // ============================================
-// 컴포넌트 배럴 파일
+// 컴포넌트 Export 허브
 // 경로: src/components/index.ts
+// 세션 18A: 전체 컴포넌트 re-export
 // ============================================
 
 export { default as CRESTApp } from './CRESTApp';
+export { default as CrestLogo } from './CrestLogo';
 export { default as ResponsiveHeader } from './ResponsiveHeader';
 export { default as ResponsiveSummaryCards } from './ResponsiveSummaryCards';
 export { default as MobileBottomNav } from './MobileBottomNav';
 export { default as MarketMiniSummary } from './MarketMiniSummary';
+export { default as EnhancedMiniChart } from './EnhancedMiniChart';
 export { default as MarketCycleWidget } from './MarketCycleWidget';
-export { default as MiniChart } from './MiniChart';
+export { default as BuffettIndicatorWidget } from './BuffettIndicatorWidget';
+export { default as SellMethodGuide } from './SellMethodGuide';
+export { default as PositionEditModal } from './PositionEditModal';
+export { default as AINewsSummary } from './AINewsSummary';
 export { default as PositionCard } from './PositionCard';
 export { default as AlertCard } from './AlertCard';
-export { default as SellMethodGuide } from './SellMethodGuide';
-export { default as UpgradePopup } from './UpgradePopup';
 export { default as AddStockModal } from './AddStockModal';
-export { default as CrestLogo } from './CrestLogo';
+export { default as UpgradePopup } from './UpgradePopup';
 export { default as Footer } from './Footer';
