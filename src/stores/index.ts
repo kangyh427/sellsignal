1739cdd054a14b7e7611d91b9079dd60e5994785
kangyh427 <kangyh427@gmail.com>
2026-02-08 13:28:15 +0900

    Delete src/stores directory

--- src/stores/index.ts
+++
@@ -1,8 +0,0 @@
-// ============================================
-// 스토어 배럴 파일
-// 위치: src/stores/index.ts
-// ============================================
-
-export { usePositionStore } from './usePositionStore';
-export { useAppStore } from './useAppStore';
-export { useUserStore } from './useUserStore';
